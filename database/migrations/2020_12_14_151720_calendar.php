0c98bd395606a5d1c4c0bc7be69983680a003483
Rafael Benosa <benosa.rafael@gmail.com>
2020-12-14 23:38:54 +0800

    initial commit

---
+++ database/migrations/2020_12_14_151720_calendar.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class Calendar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_title')->nullable();
+            $table->date('date');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+}
