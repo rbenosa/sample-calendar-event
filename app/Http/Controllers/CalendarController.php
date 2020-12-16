f10ec5caaacfab7e293e8e142ee3189412081a34
Rafael Benosa <benosa.rafael@gmail.com>
2020-12-16 10:54:05 +0800

    minor update

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -55,7 +55,7 @@ class CalendarController extends Controller
             'day'           => [
 
                                 'required',
-                                'min:2'
+                                'min:1'
                             ]
 
         ],
